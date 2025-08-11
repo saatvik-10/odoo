a261c9b0cf6ff27cdce429a4de92863a620c9611
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-11 13:45:40 +0530

    updated merge error

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-export { EndUserNavbar } from './navbars/VendorNavbar';
-=======
 export { VendorNavbar } from './navbars/VendorNavbar';
->>>>>>> cce13089031097221365acfce263a06637fc9158
 export { CustomerNavbar } from './navbars/CustomerNavbar';
