7babb28b689ee3ed0f61e492cbad3d6bd89b7822
Infernus04 <magardeishan@gmail.com>
2025-08-11 12:55:07 +0530

    address validator bug resolve

--- backend/src/validators/address.validator.ts
+++ backend/src/validators/address.validator.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const addressValidator = z.object({
     name : z.string(),
     addressLine1 : z.string().min(3, "Address line 1 is required"),
-    addressLine2 : z.string().optional(),
     street : z.string().min(3, "Street is required"),
     pincode : z.string().min(6, "Pincode is required").max(6, "Pincode must be 6 characters"),
     city : z.string().min(2, "City is required"),
